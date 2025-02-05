8daaec61b0095184349f1a790ed320ac926f6b50
PRAKASH ANAND <134529932+prakashanand084@users.noreply.github.com>
2025-02-05 07:40:25 +0530

    Update app.py
    2nd time aap.py updated

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-def add_numbers(a, b, c):
-    return a + b +c
+def add_numbers(a, b, c, d):
+    return a + b + c + d
 
 if __name__ == "__main__":
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
     num3 = float(input("Enter third number: "))
+    num3 = float(input("Enter third number: "))
 
-    result = add_numbers(num1, num2, num3)
-    print(f"The sum of {num1},  {num2} and {num3} is {result}")
+    result = add_numbers(num1, num2, num3, num4)
+    print(f"The sum of {num1},  {num2}, {num3} and {num4} is {result}")
